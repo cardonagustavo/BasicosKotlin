81e362cd1ecfede4fa2d4282ea0769fa249ab958
Gus <cardona50368@gmail.com>
2023-09-25 17:17:58 -0500

    Se añade juego sencillo de adivinar numero

--- app/src/main/java/com/example/introdos/CondicionalIf.kt
+++ app/src/main/java/com/example/introdos/CondicionalIf.kt
@@ -35,7 +35,7 @@ fun main () {
      * Este programa es una calculadora simple que permite al usuario ingresar dos números
      * y realizar operaciones de suma, resta, multiplicación o división.
      */
-    fun main() {
+
         // Solicitar al usuario ingresar dos números
         println("Ingrese el primer número:")
         val num1 = readLine()!!.toDouble()
@@ -63,7 +63,37 @@ fun main () {
         } else {
             println("Operación no válida.")
         }
+
+    //--------------------------------------------------------------//
+
+    /**
+     * Este programa es un juego simple en el que el usuario debe adivinar un número aleatorio
+     * entre 1 y 100. El programa proporciona pistas sobre si el número es demasiado alto o bajo
+     * y muestra la cantidad de intentos necesarios para adivinar el número.
+     */
+
+    //Generar u numero aleatorio entre 1 y 10
+    val numeroAleatorio = (1..10).random()
+    var intentos = 0
+
+    //Juego de adivinar el numero
+    while (true){
+        println("Adivina el numero de 1 a 10")
+        val intento = readLine()!!.toInt()
+        intentos++
+
+        if (intento == numeroAleatorio) {
+            println("Adivinaste el numero en $intentos intentos")
+            break
+        }else if (intento < numeroAleatorio) {
+            println("Casi, pero te pasaste, intenta de nuevo")
+        }else{
+            println("Casi, pero te quedaste por debajo, intenta de nuevo")
+        }
+
     }
 
 
 }
+
+
