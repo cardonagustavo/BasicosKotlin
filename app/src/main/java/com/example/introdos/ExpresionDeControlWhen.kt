017ad59518c8ee9b6ef1ca884b7febb2712e293a
Gus <cardona50368@gmail.com>
2023-10-10 08:38:38 -0500

    Practica colecciones

---
+++ app/src/main/java/com/example/introdos/ExpresionDeControlWhen.kt
@@ -0,0 +1,37 @@
+package com.example.introdos
+
+import java.util.Locale
+
+/*
+Este pequeño ejercicio nos permite convertir una temperatura
+ */
+/*
+fun main () {
+    //El usuario ingresa la temperatura
+    println("Convertir temperatura Celsius a Fahrenheit o Fahrenheit a Celsius")
+    println("Ingresa la temperatura a convertir")
+
+    val temperatura = readLine()!!.toDouble()
+
+    println("Ingrese la unidad de medida de temperatura (C o F)")
+    val unidad = readLine()?.trim()?.toLowerCase()
+
+    val resultado = when (unidad) {
+        1.toString() -> "c" {
+            val fahrenheit = (temperatura * 9 / 5) + 32
+
+        "$temperatura grados Celcius son $fahrenheit grados Fahrenheit"
+    }
+        2.toString() -> "f" {
+        val celcius = (temperatura - 32) * 9 / 5
+        "$temperatura grados Fahrenheit son $celcius grados Celcius."
+
+    }
+
+        else -> "Unidad de temperatura no valida"
+    }
+
+    println(resultado)
+}
+
+ */
---
+++ app/src/main/java/com/example/introdos/Listas.kt
@@ -0,0 +1,52 @@
+package com.example.introdos
+
+fun main() {
+    val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
+    println("list:   ${numbers}")
+    println("sorted: ${numbers.sorted()}")
+
+    val setOfNumbers = numbers.toSet()
+    println("set:    ${setOfNumbers}")
+
+    val set1 = setOf(1,2,3)
+    val set2 = mutableSetOf(3,2,1)
+    println("$set1 == $set2: ${set1 == set2}")
+
+    println("contains 7: ${setOfNumbers.contains(7)}")
+
+    val conjunto1 = setOf(1,2,37,4,50,6,7,87,9,0)
+    val conjunto2 = setOf(3,5,6,87,2)
+
+    val interseccion = conjunto1.intersect(conjunto2)
+    println("interseccion: $interseccion")
+
+
+    val union = conjunto1.union(conjunto2)
+    println("union: $union")
+
+    val conjuntoA = setOf("manzana", "plátano", "naranja")
+    val conjuntoB = setOf("naranja", "uva", "pera")
+
+    val interseccionString = conjuntoA.intersect(conjuntoB)
+    val unionString = conjuntoA.union(conjuntoB)
+
+    println("Intersección: $interseccionString")
+    println("Unión: $unionString")
+
+    val peopleAges = mutableMapOf<String, Int>(
+        "Diego" to 40,
+        "Master" to 33,
+        "Gus" to 28
+    )
+    peopleAges.put("Barbara", 42)
+    peopleAges["Joe"] = 51
+    peopleAges["Fred"] = 31
+    println(peopleAges)
+
+    peopleAges.forEach { print("${it.key} is ${it.value}, ") }
+
+    println(peopleAges.map { "${it.key} is ${it.value}" }.joinToString(", ") )
+
+    val filteredNames = peopleAges.filter { it.key.length < 4 }
+    println(filteredNames)
+}
