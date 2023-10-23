d4c41cd4675b7e406bc092ffd627bf244908f614
Gus <cardona50368@gmail.com>
2023-10-23 16:59:33 -0500

    Null Safety

---
+++ app/src/main/java/com/example/introdos/Classes/Nota.kt
@@ -0,0 +1,14 @@
+package com.example.introdos
+
+import java.util.Date
+
+class Nota {
+
+    class Nota {
+        var nombre: String = ""
+        var contenido: String = ""
+        var fechaCreacion: Date = Date()
+
+
+    }
+}
---
+++ app/src/main/java/com/example/introdos/Debugging.kt
@@ -0,0 +1,4 @@
+package com.example.introdos
+
+class Debugging {
+}
